b7ca4745b1409bad2c88833722b26ef614c81ecb
KelvinoKing <kelvinoking@outlook.com>
2023-11-22 16:04:56 +0000

    Fix

--- models/amenity.py
+++ models/amenity.py
@@ -5,17 +5,7 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel, Base):
+class Amenity(BaseModel):
     """Amenity class for all amenities
     """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-
-    place_amenities = Table('place_amenities', Base.metadata, Column(
-        'place_id', String(60), ForeignKey('place.id'), primary_key=True),
-        Column(
-            'amenity_id', String(60), ForeignKey(
-                'amenities.id'), primary_key=True))
-
-    place = relationship(
-            'Place', secondary=place_amenities, back_populates='amenities')
+    name = ""
