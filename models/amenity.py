62acd36f168943bd6473a4cbef906d34b978081e
J-Derrick <j.derrickwaweru@gmail.com>
2023-11-23 05:58:16 +0300

    DBStorage - Amenity... and BOOM!

--- models/amenity.py
+++ models/amenity.py
@@ -5,20 +5,14 @@ from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
 
-place_amenity = Table('place_amenity', Base.metadata, Column(
-    'place_id', String(60), ForeignKey(
-        'places.id'), primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey(
-        'amenities.id'), primary_key=True, nullable=False),
-    extend_existing=True
-        )
-
-
 class Amenity(BaseModel, Base):
     """Amenity class for all amenities
     """
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
 
-    place_amenities = relationship(
-            'Place', secondary=place_amenity, backref='amenities')
+place_amenity = Table('place_amenity', Base.metadata, 
+    Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False),
+    extend_existing=True
+)
