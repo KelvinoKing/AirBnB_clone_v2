5f56180b1c6e46fdfae6d9e823cbeca0994e8f34
KelvinoKing <kelvinoking@outlook.com>
2023-12-07 06:36:54 +0000

    Fixes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,65 +1,47 @@
 #!/usr/bin/python3
-"""import fabric, os
 """
-from fabric.api import run, put, env
-import os
+a Module that contains a do_deploy function.
+"""
 
+from fabric.api import env, run, put
+from os.path import exists
 
 env.hosts = ['52.200.50.96', '52.201.160.72']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers
-
-    Args:
-        archive_path (str): Path to archive to be deployed
-
-    Returns:
-        bool: True if all operations are done rght else false
     """
-    if not os.path.exists(archive_path):
+    A Fabric script that distributes an archive to your web servers
+    """
+
+    # Check if the archive file exists
+    if not exists(archive_path):
         return False
 
     try:
-        # Upload the archive to /tmp/ dir on the web server
+        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
 
-        # Uncompress the archive to /data/web_static/releases/<filename
-        # without extension> on the web server
-        archive_filename = os.path.basename(archive_path)
-        release_path = "/data/web_static/releases/{}".format(
-                archive_filename.split('.')[0])
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_filename, release_path))
+        # Extract the archive to /data/web_static/releases/
+        # <archive filename without extension>
+        filename = archive_path.split('/')[-1]
+        folder_name = f"/data/web_static/releases/{filename.split('.')[0]}"
+        run(f"mkdir -p {folder_name}")
+        run(f"tar -xzf /tmp/{filename} -C {folder_name}")
 
         # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_filename))
+        run(f"rm /tmp/{filename}")
 
-        # Create a new symbolic link and delete the old one
-        current_link = '/data/web_static/current'
-        run('rm -f {}'.format(current_link))
-        run('ln -s {} {}'.format(release_path, current_link))
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -f /data/web_static/current")
 
-        print("New version deployed!")
+        # Create a new symbolic link /data/web_static/current
+        # linked to the new version
+        run(f"ln -s {folder_name} /data/web_static/current")
+
         return True
 
     except Exception as e:
+        print(e)
         return False
-
-
-if __name__ == "__main__":
-    import sys
-    import argparse
-
-    args = sys.argv[1:]
-    for i in range(len(args)):
-        if args[i] == '-i' and i + 1 < len(args):
-            env.key_filename = args[i + 1]
-        elif args[i] == '-u' and i + 1 < len(args):
-            env.user = args[i + 1]
-
-    parser = argparser.ArgumentParser()
-    parser.add_argument('--archive_path', required=True)
-    args = parser.parse_args()
-
-    do_deploy(args.archive_path)
