d5dbd22c3030555c054d35f174b7b179c03d2b07
KelvinoKing <kelvinoking@outlook.com>
2023-12-07 08:31:54 +0000

    Full deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,13 +28,26 @@ sudo chown -R ubuntu:ubuntu /data/
 
 nginx_config='/etc/nginx/sites-available/default'
 echo "server {
-       listen 80;
-       listen [::]:80;
-       server_name _;
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-       location /hbnb_static {
-         alias /data/web_static/current/;
-       }
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
 }" | sudo tee $nginx_config > /dev/null
 
 # Restart nginx
