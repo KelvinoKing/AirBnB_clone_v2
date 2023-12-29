f3ff82d9908374394c642b456e7c70ef7b983c17
KelvinoKing <kelvinoking@outlook.com>
2023-12-29 09:35:26 +0000

    Set up web static

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,6 +1,3 @@
-# Configures a web server for deployment of web_static.
-
-# Nginx configuration file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
@@ -26,66 +23,66 @@ $nginx_conf = "server {
 
 package { 'nginx':
   ensure   => 'present',
-  provider => 'apt'
+  provider => 'apt',
 } ->
 
 file { '/data':
-  ensure  => 'directory'
+  ensure  => 'directory',
 } ->
 
 file { '/data/web_static':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/releases':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/shared':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
+  content => "Holberton School Puppet\n",
 } ->
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
+  target => '/data/web_static/releases/test',
 } ->
 
 exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+  path => '/usr/bin/:/usr/local/bin/:/bin/',
 }
 
 file { '/var/www':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/var/www/html':
-  ensure => 'directory'
+  ensure => 'directory',
 } ->
 
 file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "Holberton School Nginx\n"
+  content => "Holberton School Nginx\n",
 } ->
 
 file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
+  content => "Ceci n'est pas une page\n",
 } ->
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
-  content => $nginx_conf
+  content => $nginx_conf,
 } ->
 
 exec { 'nginx restart':
-  path => '/etc/init.d/'
+  path => '/etc/init.d/',
 }
