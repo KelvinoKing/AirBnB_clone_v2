f21fdb1c01ed3bcb3f180f1021267ad32a37ecfb
J-Derrick <j.derrickwaweru@gmail.com>
2024-01-04 19:07:04 +0300

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,16 +3,13 @@
 """
 from flask import Flask
 
+app = Flask(_name_)
 
-app = Flask(__name__)
-
-
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Function for the home page
     """
-    return "Hello HBNB!"
-
+   return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
